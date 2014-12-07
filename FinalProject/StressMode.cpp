1166c1d9da29db6a1a42064282db8dcb80e67299
DNguyen17 <danhn@smu.edu>
2014-12-07 04:55:25 -0600

    stress test added

--- FinalProject/StressMode.cpp
+++ FinalProject/StressMode.cpp
@@ -2,4 +2,135 @@
 
 StressMode::StressMode()
 {
+
+}
+
+StressMode:: ~StressMode()
+{
+
+}
+
+void StressMode::docFileParser(string& fileName)
+{
+
+    ifstream ifs;
+    ifs.open(fileName);
+
+    if(!ifs){
+        cerr << "cannot open file " << fileName << endl;
+        exit(1);
+    }
+
+    string flag;
+
+    while (!ifs.eof()){
+        ifs >> flag;
+
+        if (flag == "CI"){
+            cout << "clearIndex" << endl;
+            interact.clearIndex();
+            myParser.clearIndex();
+        }
+
+        else if(flag == "BI"){
+            cout << "build index from memory" << endl;
+            //choose 1 for AVL tree and 0 for HashTable
+            int userChoice;
+            //get the int following
+            ifs>>userChoice;
+            structureChoice = userChoice;
+
+            interact.createIndex(userChoice);
+            //create the index of the XML parser of Index Handler
+            myParser.createIndex(userChoice);
+
+
+
+
+        }
+
+        else if (flag == "IF"){
+            cout << "store off new file" << endl;
+            string newData;
+            ifs>>newData;
+            myParser.storeOffNewData(newData);
+            char stressOutput[] = "StressTestIndex.txt";
+            myParser.storeOffIndex(stressOutput);
+
+            //need to reload index into index handler of
+            //interactive mode
+            interact.setInputFileForIndex(stressOutput);
+            interact.buildIndexFromMemory(structureChoice);
+        }
+
+        else if (flag == "SE"){
+            cout << "search" << endl;
+
+            getline(ifs,arguments);
+            interact.processQuery(arguments);
+        }
+
+        else if (flag == "PP"){
+            cout << "Pick Page" << endl;
+            int userPage;
+            ifs >>userPage;
+
+            interact.displayText(userPage);
+
+
+        }
+        else if (flag == "NS"){
+            cout<< "new Search"<<endl;
+            interact.clearAllCurrAndFinalMembers();
+
+        }
+
+        else if (flag == "ST"){
+            cout << "start time" << endl;
+            startTime();
+        }
+
+        else if (flag == "ET"){
+            cout << "stop time" << endl;
+            stopTime();
+        }
+
+        else if (flag == "PT"){
+            cout << "print time" << endl;
+            printTime();
+        }
+
+        else {
+            cout << "invalid" << endl;
+        }
+        //make sure goes to next line
+        string dummy;
+        getline(ifs,dummy);
+    }
+
+}
+
+void StressMode::stressUI()
+{
+    string fileName;
+    cout << "Enter file name: ";
+    getline(cin, fileName);
+
+    docFileParser(fileName);
+}
+
+void StressMode::startTime()
+{
+    start = std::chrono::system_clock::now();
+}
+
+void StressMode::stopTime()
+{
+    end = std::chrono::system_clock::now();
+}
+
+void StressMode::printTime()
+{
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    cout << "Duration: " << elapsed_seconds.count() << " seconds" << endl;
 }
--- FinalProject/StressMode.h
+++ FinalProject/StressMode.h
@@ -1,10 +1,52 @@
 #ifndef STRESSMODE_H
 #define STRESSMODE_H
+#include "MaintMode.h"
+#include "InteractiveMode.h"
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+/* Function Flags:
+ * CI: clearIndex()
+ * BI: buildIndexFromMemory(int)
+ * IF: storeOffNewData(string)
+ * SE: processQuery(string)
+ * PP: pickPage(int)
+ * ST: startTime
+ * ET: stopTime
+ * PT: printTime
+ */
 
 class StressMode
 {
+private:
+    MaintMode maint;
+    InteractiveMode interact;
+    XMLParser myParser;
+    int structureChoice;
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
 public:
     StressMode();
+    ~StressMode();
+
+    //! reads file and execute commands, displaying times
+    void docFileParser(string& fileName);
+
+    //! Interact with user asking for fileName to stress test
+    void stressUI();
+
+    //! Start Timer
+    void startTime();
+
+    //! Stop Timer
+    void stopTime();
+
+    //! Print Duration
+    void printTime();
+
 };
 
 #endif // STRESSMODE_H
